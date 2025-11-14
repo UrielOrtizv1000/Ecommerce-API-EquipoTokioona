1a4764bcb90f83852cfc77d79c4ce034afe56257
Oscar Gomez <oscarruiz6448@hotmail.com>
2025-11-14 13:45:24 -0600

    Signup: Add new user to DB with encrypted password

--- back/models/User.js
+++ back/models/User.js
@@ -8,3 +8,12 @@
  *   - Never returns the hashed password in queries
  * Required by: Auth controller, login, register, forgot-password
  */
+const pool = require("../db/conexion");
+
+// Insert new user into database (Sign-up)
+async function createUser(name, email, password) {
+  const [res] = await pool.query('INSERT INTO usuarios (nombre, email, contrasena) VALUES (?, ?, ?)', [name, email, password]);
+  return res.insertId;
+}
+
+module.exports = { createUser };
--- back/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signup } = require("../controllers/auth.controller");
-
-const router = express.Router();
-
-// POST /api/signup
-router.post("/signup", signup);
-
-module.exports = router;
