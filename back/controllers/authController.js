201a60782769cb7c14c5d6e3547d45e2d96a971b
Oscar Gomez <oscarruiz6448@hotmail.com>
2025-11-19 22:16:15 -0600

    feat: session management with JWT

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -6,8 +6,11 @@ Lockout: Uses FailedLogin table with last_attempt and attempts.
 Forgot Password: Generates a unique token, sends an email with a reset link.
 CAPTCHA: Generated in captchaController, validated here.
 */
+const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const { storeToken, disposeToken } = require("../middlewares/authMiddleware");
+
 
 // -- USER REGISTER CONTROLLER (SIGNUP) --
 const signup = async (req, res) => {
@@ -47,4 +50,66 @@ const signup = async (req, res) => {
   }
 }
 
-module.exports = { signup }
+// -- USER LOGIN CONTROLLER --
+const login = async (req,res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if one or more form entries are empty
+    if (!username || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "One or more form entries are empty"
+      });
+    }
+
+    const userData = await User.userLogin(username, password);
+    if (!userData) {
+      return res.status(401).json({
+        success: false,
+        message: "Wrong credentials"
+      });
+    }
+
+    const token = storeToken(userData);
+
+    res.status(200).json({
+      success: true,
+      token: token,
+    });
+  } catch (error) {
+    console.error("Login error: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
+}
+
+// -- USER LOGOUT CONTROLLER --
+const logout = async (req, res) => {
+  try {
+    const token = req.token;
+    const dispose = disposeToken(token);
+
+    if (!dispose) {
+      return res.status(404).json({
+        success: false,
+        message: "Token was not found"
+      });
+    }
+    
+    res.status(200).json({
+      success: true,
+      message: "Token disposed succesfully"
+    });
+  } catch (error) {
+    console.error("Logout error: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
+}
+
+module.exports = { signup, login, logout }
--- back/models/User.js
+++ back/models/User.js
@@ -8,6 +8,7 @@
  *   - Never returns the hashed password in queries
  * Required by: Auth controller, login, register, forgot-password
  */
+const bcrypt = require("bcryptjs");
 const pool = require("../db/conexion");
 
 // Insert new user into database (Sign-up)
@@ -22,4 +23,47 @@ async function createUser(name, email, password) {
   return res.insertId;
 }
 
-module.exports = { createUser };
+// User login
+async function userLogin(name, password) {
+  // Get user data (password is required to verify with bcrypt)
+  const [check] = await pool.query('SELECT user_id, name, email, role, password FROM users WHERE name = ?', [name])
+
+  if (check.length === 0)
+    return null;
+
+  // Set user
+  const user = check[0];
+
+  // Verify bcrypt's encrypted password
+  const verifyHash = await bcrypt.compare(password, user.password);
+
+  if (!verifyHash) {
+    // Iterate failed attempts
+    const [fail] = await pool.query('UPDATE users SET failed_attempts = failed_attempts + 1 WHERE user_id = ?', [user.user_id]);
+
+    // Get current failed attempts value
+    const [currAtt] = await pool.query('SELECT failed_attempts FROM users WHERE user_id = ?', [user.user_id]);
+
+    // If current failed attempts value is divisible by 3, set lockout
+    if ((currAtt[0].failed_attempts % 3) === 0) {
+      const [lock] = await pool.query('UPDATE users SET lockout_date = CURRENT_TIMESTAMP WHERE user_id = ?', [user.user_id]);
+    }
+
+    return null;
+  }
+
+  // Remove failed attempts
+  const [remFail] = await pool.query('UPDATE users SET failed_attempts = 0 WHERE user_id = ?', [user.user_id]);
+
+  // Remove lockout, if any
+  const [unlock] = await pool.query('UPDATE users SET lockout_date = NULL WHERE user_id = ?', [user.user_id]);
+
+  // Return public data
+  return {
+    id: user.user_id,
+    email: user.email,
+    role: user.role
+  };
+}
+
+module.exports = { createUser, userLogin };
